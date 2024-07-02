690d04b323e84b0edb4d1771907f236493e0a3d7
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-02 13:33:54 +0300

    "Added new CSS styles for a fixed button and form, and updated the layout of the master blade template."

--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -5,7 +5,7 @@
 @include('client.layouts.partials.footer')
   <!-- "Bilgi Al" butonu ve form -->
   <div id="infoButton" class="info-button">Bilgi Al</div>
-
+  <div class="right-fixed-btn d-none d-lg-block"><span><i class="fa fa-envelope-o me-3" aria-hidden="true"></i> Başvuru Formu</span></div>
   <div id="formContainer" class="contact-form-style-01">
       <div class="ps-8 pe-8 pt-8 pb-8 lg-p-8 bg-white box-shadow-quadruple-large border-radius-6px">
           <h6 class="d-inline-block fw-600 text-dark-gray ls-minus-1px mb-35px sm-mb-25px">
@@ -49,7 +49,98 @@
   </div>
 
 <style>
-    /* Bilgi Al butonu stili */
+
+.right-fixed-btn {
+                position: fixed;
+                z-index: 9;
+                right: -105px;
+                top: 50%;
+                transform: rotate(90deg);
+                cursor: pointer;
+                animation: pulse3 1.5s infinite;
+                border-radius: 0px 0px 20px 20px;
+                border: 1px solid #000;
+                background: #2a2a2d;
+                padding: 10px 35px;
+            }
+
+            .right-fixed-btn:hover {
+                background: #ef0003
+            }
+
+            .right-fixed-btn span {
+                color: #fff;
+                font-size: 20px;
+                text-decoration: 0px 0px 5px #000;
+            }
+
+            .right-fixed-form {
+                border-radius: 10px 0px 0px 10px;
+                line-height: 20px;
+                width: 350px;
+                position: fixed;
+                right: -430px;
+                box-sizing: border-box;
+                background-color: #fff;
+                z-index: 99999;
+                height: auto;
+                display: table;
+                top: 50%;
+                transition: all .6s cubic-bezier(.42,0,.07,.99);
+                transform: translateY(-50%);
+                box-shadow: -1px 1px 19px 6px #00000024;
+            }
+
+            .right-form {
+                width: 350px;
+                padding: 30px;
+                position: relative;
+            }
+
+            .right-form-close {
+                position: absolute;
+                top: -10px;
+                left: -15px;
+                display: flex;
+                justify-content: center;
+                cursor: pointer;
+                align-items: center;
+                width: 35px;
+                height: 35px;
+                background: #ef0003;
+                color: #fff;
+                border-radius: 50%;
+            }
+
+            .right-form-close:hover {
+                background: #2a2a2d;
+            }
+
+            .right-0 {
+                right: 0;
+            }
+
+            .me-3 {
+                margin-right: 15px;
+            }
+
+            /* Animasyon */
+            @keyframes pulse3 {
+                0% {
+                    -moz-box-shadow: 0 0 0 0 rgba(255,255,255,.5);
+                    box-shadow: 0 0 0 0 rgb(0 0 0 / 50%);
+                }
+
+                70% {
+                    -moz-box-shadow: 0 0 0 30px rgba(255,255,255,0);
+                    box-shadow: 0 0 0 30px rgb(0 0 0 / 0%);
+                }
+
+                100% {
+                    -moz-box-shadow: 0 0 0 0 rgba(255,255,255,0);
+                    box-shadow: 0 0 0 0 rgb(0 0 0 / 0%);
+                }
+            }
     .info-button {
         position: fixed;
         top: 50%;
@@ -68,7 +159,7 @@
     .contact-form-style-01 {
         position: fixed;
         top: 50%;
-        right: -400px; 
+        right: -400px;
         transform: translateY(-50%);
         width: 400px;
         transition: right 0.3s ease;
