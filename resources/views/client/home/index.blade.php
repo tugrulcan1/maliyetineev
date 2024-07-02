ebeb633c38027c31aff7690b13fc95a6c539d6e1
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-02 16:14:35 +0300

    "Added new section with title, images, and testimonials slider, and removed duplicate section"

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -20,6 +20,8 @@
      @endforeach
 
 
+
+
      <section class="pt-0 mgyy  " style="display:none">
          <div class="container">
              <div class="row align-items-center justify-content-center">
@@ -94,6 +96,116 @@
         </div>
     </section> --}}
 
+
+    <div class="container mt-5">
+
+        <h3 class="text-dark-gray fw-700" style="text-align: center;">{{ $settings['section_8_title'] }}</h3>
+        <span style="margin-left:130px " class="fw-600">{{ $settings['section_8_small_title'] }}</span>
+        <div class="row mt-3">
+
+            <div class="col-sm  mb-4" style="text-align: center;">
+                <div>
+                    <img src="/images/trust.png" alt="" style="height: 100px;">
+                    <p class="text-dark-gray fw-700 baslikP"
+                        style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
+                        {{ $settings['section_8_1'] }}</p>
+                    <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
+                        {{ $settings['section_8_1.1'] }}</p>
+                </div>
+            </div>
+            <div class="col-sm  mb-4" style="text-align: center;">
+                <div>
+                    <img src="/images/budget.png" alt="" style="height: 100px; ">
+                    <p class="text-dark-gray fw-700"
+                        style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
+                        {{ $settings['section_8_2'] }}
+                    </p>
+                    <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
+                        {{ $settings['section_8_2.1'] }}</p>
+                </div>
+            </div>
+            <div class="col-sm  mb-4" style="text-align: center;">
+                <div>
+                    <img src="/images/bank.png" alt="" style="height: 100px; ">
+                    <p class="text-dark-gray fw-700"
+                        style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
+                        {{ $settings['section_8_3'] }}
+                    </p>
+                    <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
+                        {{ $settings['section_8_3.1'] }}</p>
+                </div>
+            </div>
+            <div class="col-sm  mb-4" style="text-align: center;">
+                <div>
+                    <img src="/images/timing.png" alt="" style="height: 100px;">
+                    <p class="text-dark-gray fw-700"
+                        style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
+                        {{ $settings['section_8_4'] }}
+                    </p>
+                    <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
+                        {{ $settings['section_8_4.1'] }}</p>
+                </div>
+            </div>
+            <div class="col-sm  mb-4" style="text-align: center;">
+                <div>
+                    <img src="/images/smartphone.png" alt="" style="height: 100px; ">
+                    <p class="text-dark-gray fw-700"
+                        style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
+                        {{ $settings['section_8_5'] }}</p>
+                    <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
+                        {{ $settings['section_8_5.1'] }}</p>
+                </div>
+            </div>
+        </div>
+        <div class="row m-0 align-items-center justify-content-center border border-color-extra-medium-gray border-radius-100px md-border-radius-6px ps-10px pe-10px box-shadow-quadruple-large"
+            data-anime="{ &quot;scale&quot;: [1.1, 1], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 800, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
+            <div class="col-lg-10">
+                <div class="swiper slider-one-slide testimonials-style-09"
+                    data-slider-options="{ &quot;slidesPerView&quot;: 1, &quot;loop&quot;: true, &quot;autoplay&quot;: { &quot;delay&quot;: 4000, &quot;disableOnInteraction&quot;: false }, &quot;navigation&quot;: { &quot;nextEl&quot;: &quot;.slider-one-slide-next-1&quot;, &quot;prevEl&quot;: &quot;.slider-one-slide-prev-1&quot; }, &quot;keyboard&quot;: { &quot;enabled&quot;: true, &quot;onlyInViewport&quot;: true }, &quot;effect&quot;: &quot;slide&quot; }">
+                    <div class="swiper-wrapper align-items-center">
+
+                        @foreach ($comments as $comment)
+                            <div class="swiper-slide">
+                                <div class="row align-items-center pt-25px pb-25px">
+                                    <div class="col-lg-8 d-lg-flex align-items-center text-center text-lg-start">
+                                        <img src="images/yildiz.png" class="rounded-circle w-100px h-100px md-mb-35px"
+                                            alt style="object-fit: cover !important;">
+                                        <span style=""
+                                            class="d-block ps-40px md-ps-0 md-mx-auto position-relative">{!! $comment->content !!}</span>
+                                    </div>
+                                    <div class="col-lg-1 d-none d-lg-inline-block">
+                                        <div
+                                            class="separator-line w-1px md-w-100 h-60px md-h-1px bg-extra-medium-gray mx-auto">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 text-center text-lg-start md-mt-15px">
+                                        <span
+                                            class="fs-19 ls-minus-05px fw-600 text-dark-gray d-block lh-28">{{ $comment->full_name }}</span>
+                                        <div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 md-mb-25px">
+                <div class="d-flex justify-content-center">
+
+                    <div class="slider-one-slide-prev-1 swiper-button-prev slider-navigation-style-04 bg-very-light-gray">
+                        <i class="fa-solid fa-arrow-left icon-small text-dark-gray"></i>
+                    </div>
+                    <div class="slider-one-slide-next-1 swiper-button-next slider-navigation-style-04 bg-very-light-gray">
+                        <i class="fa-solid fa-arrow-right icon-small text-dark-gray"></i>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 
      <section class="overflow-hidden" style="padding:0px 40px !important;">
          <div class="container">
@@ -324,115 +436,7 @@
 
      </section>
 
-     <div class="container mt-5">
 
-         <h3 class="text-dark-gray fw-700" style="text-align: center;">{{ $settings['section_8_title'] }}</h3>
-         <span style="margin-left:130px " class="fw-600">{{ $settings['section_8_small_title'] }}</span>
-         <div class="row mt-3">
-
-             <div class="col-sm  mb-4" style="text-align: center;">
-                 <div>
-                     <img src="/images/trust.png" alt="" style="height: 100px;">
-                     <p class="text-dark-gray fw-700 baslikP"
-                         style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
-                         {{ $settings['section_8_1'] }}</p>
-                     <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
-                         {{ $settings['section_8_1.1'] }}</p>
-                 </div>
-             </div>
-             <div class="col-sm  mb-4" style="text-align: center;">
-                 <div>
-                     <img src="/images/budget.png" alt="" style="height: 100px; ">
-                     <p class="text-dark-gray fw-700"
-                         style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
-                         {{ $settings['section_8_2'] }}
-                     </p>
-                     <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
-                         {{ $settings['section_8_2.1'] }}</p>
-                 </div>
-             </div>
-             <div class="col-sm  mb-4" style="text-align: center;">
-                 <div>
-                     <img src="/images/bank.png" alt="" style="height: 100px; ">
-                     <p class="text-dark-gray fw-700"
-                         style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
-                         {{ $settings['section_8_3'] }}
-                     </p>
-                     <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
-                         {{ $settings['section_8_3.1'] }}</p>
-                 </div>
-             </div>
-             <div class="col-sm  mb-4" style="text-align: center;">
-                 <div>
-                     <img src="/images/timing.png" alt="" style="height: 100px;">
-                     <p class="text-dark-gray fw-700"
-                         style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
-                         {{ $settings['section_8_4'] }}
-                     </p>
-                     <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
-                         {{ $settings['section_8_4.1'] }}</p>
-                 </div>
-             </div>
-             <div class="col-sm  mb-4" style="text-align: center;">
-                 <div>
-                     <img src="/images/smartphone.png" alt="" style="height: 100px; ">
-                     <p class="text-dark-gray fw-700"
-                         style="font-size: 20px; margin: 0; padding: 0; color: #333; font-size: 16px;">
-                         {{ $settings['section_8_5'] }}</p>
-                     <p style="font-size: 12px;font-weight: normal;line-height: 20px;color: #647589">
-                         {{ $settings['section_8_5.1'] }}</p>
-                 </div>
-             </div>
-         </div>
-         <div class="row m-0 align-items-center justify-content-center border border-color-extra-medium-gray border-radius-100px md-border-radius-6px ps-10px pe-10px box-shadow-quadruple-large"
-             data-anime="{ &quot;scale&quot;: [1.1, 1], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 800, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
-             <div class="col-lg-10">
-                 <div class="swiper slider-one-slide testimonials-style-09"
-                     data-slider-options="{ &quot;slidesPerView&quot;: 1, &quot;loop&quot;: true, &quot;autoplay&quot;: { &quot;delay&quot;: 4000, &quot;disableOnInteraction&quot;: false }, &quot;navigation&quot;: { &quot;nextEl&quot;: &quot;.slider-one-slide-next-1&quot;, &quot;prevEl&quot;: &quot;.slider-one-slide-prev-1&quot; }, &quot;keyboard&quot;: { &quot;enabled&quot;: true, &quot;onlyInViewport&quot;: true }, &quot;effect&quot;: &quot;slide&quot; }">
-                     <div class="swiper-wrapper align-items-center">
-
-                         @foreach ($comments as $comment)
-                             <div class="swiper-slide">
-                                 <div class="row align-items-center pt-25px pb-25px">
-                                     <div class="col-lg-8 d-lg-flex align-items-center text-center text-lg-start">
-                                         <img src="images/yildiz.png" class="rounded-circle w-100px h-100px md-mb-35px"
-                                             alt style="object-fit: cover !important;">
-                                         <span style=""
-                                             class="d-block ps-40px md-ps-0 md-mx-auto position-relative">{!! $comment->content !!}</span>
-                                     </div>
-                                     <div class="col-lg-1 d-none d-lg-inline-block">
-                                         <div
-                                             class="separator-line w-1px md-w-100 h-60px md-h-1px bg-extra-medium-gray mx-auto">
-                                         </div>
-                                     </div>
-                                     <div class="col-lg-3 text-center text-lg-start md-mt-15px">
-                                         <span
-                                             class="fs-19 ls-minus-05px fw-600 text-dark-gray d-block lh-28">{{ $comment->full_name }}</span>
-                                         <div></div>
-                                     </div>
-                                 </div>
-                             </div>
-                         @endforeach
-
-
-
-                     </div>
-                 </div>
-             </div>
-             <div class="col-lg-2 md-mb-25px">
-                 <div class="d-flex justify-content-center">
-
-                     <div class="slider-one-slide-prev-1 swiper-button-prev slider-navigation-style-04 bg-very-light-gray">
-                         <i class="fa-solid fa-arrow-left icon-small text-dark-gray"></i>
-                     </div>
-                     <div class="slider-one-slide-next-1 swiper-button-next slider-navigation-style-04 bg-very-light-gray">
-                         <i class="fa-solid fa-arrow-right icon-small text-dark-gray"></i>
-                     </div>
-
-                 </div>
-             </div>
-         </div>
-     </div>
 
      <section style="padding: 0 !important; margin-bottom: 50px;margin-top:50px; ">
          <div class="container">
