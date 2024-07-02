58ebacb88621ff2aba1623c17834481430e14d4b
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-02 15:02:58 +0300

    "Updated footer layout and styles, removed inline styles, and refactored HTML structure."

--- resources/views/client/layouts/partials/footer.blade.php
+++ resources/views/client/layouts/partials/footer.blade.php
@@ -1,94 +1,78 @@
-<footer class="footer-dark pt-0 lg-pb-35px"  style="padding-bottom: 10px !important; padding-top:10px !important;background-color:#dddddd" >
-    <div class="footer-top  sm-pt-35px sm-pb-35px border-bottom border-1 border-color-transparent-white-light" style="margin-top:20px; padding-bottom:10px !important; padding-top:0px !important;">
-        <div class="container">
-            <div class="row align-items-center justify-content-center">
-                <div class="col-xl-6 text-center text-xl-start lg-mb-30px sm-mb-20px">
-                    <h3 class="mb-5px fw-500 ls-minus-1px" style="color:#333 !important">Aklınıza takılan soru mu var?</h3>
-                    <span class="fs-20 fw-300" style="color:#333 !important">Bizimle iletişime geçebilirsiniz.</span>
-                </div>
-                <div class="col-xl-6 text-center text-xl-end">
-                    <a href="mailto:{{$settings['e-mail']}}"
-                        style="background: #0f75bc !important; color: white !important;"
-                        class="btn btn-extra-large btn-yellow left-icon btn-box-shadow btn-rounded text-transform-none d-inline-block align-middle me-15px xs-m-10px">
-                        <i class="fa fa-envelope"></i>{{$settings['e-mail']}}
-                    </a>
-                    <a href="tel:12345678910" style="background-color:rgb(230, 71, 71)"
-                        class="btn btn-extra-large btn-base-color left-icon btn-box-shadow btn-rounded d-inline-block align-middle xs-m-10px">
-                        <i class="fa fa-phone"></i>{{$settings['phone']}}
-                    </a>
-                </div>
+<footer class="footer-dark pt-3 pb-5" style="background-color: #dddddd;">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-xl-6 text-center text-xl-start mb-4 mb-sm-0">
+                <h3 class="mb-2 fw-500" style="color: #333; font-size: 24px;">Aklınıza takılan soru mu var?</h3>
+                <p class="fs-15 fw-300" style="color: #333;">Bizimle iletişime geçebilirsiniz.</p>
+            </div>
+            <div class="col-xl-6 text-center text-xl-end">
+                <a href="mailto:{{$settings['e-mail']}}" class="btn btn-extra-large btn-yellow btn-rounded text-white me-2">
+                    <i class="fa fa-envelope"></i> {{$settings['e-mail']}}
+                </a>
+                <a href="tel:{{$settings['phone']}}" class="btn btn-extra-large btn-base-color btn-rounded text-white">
+                    <i class="fa fa-phone"></i> {{$settings['phone']}}
+                </a>
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row justify-content-center fs-17 fw-300 md-mt-45px md-mb-45px xs-mt-35px xs-mb-35px" style="margin-top: 10px !important;" >
-            <div class="col-12 col-lg-3 order-sm-1 md-mb-40px xs-mb-30px last-paragraph-no-margin">
-                <a href="demo-accounting.html" class="footer-logo mb-5px d-inline-block">
-                    <img src="{{ asset('images/maliyetineevlogo.png') }}" >
+
+    <div class="container mt-4">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12 col-lg-3 mb-4 mb-lg-0">
+                <a href="demo-accounting.html" class="footer-logo mb-2 d-inline-block">
+                    <img src="{{ asset('images/maliyetineevlogo.png') }}" alt="Logo">
                 </a>
-                <span class="w-85 xl-w-95 sm-w-100" style="color:#333 !important;display:block">Maliyetine Ev Sahibi Olmak Herkesin Hakkı</span>
-                <div class="elements-social social-icon-style-02 mt-20px lg-mt-20px">
-                    <ul class="small-icon light">
-                        @foreach($socialMediaIcons as $SocialMediaIcon)
-                            <li><a class="facebook" href="{{$SocialMediaIcon->url}}" target="_blank"><i
-                                        class="fa-brands {{$SocialMediaIcon->icon_class}}"></i></a></li>
+                <p class="fs-14" style="color: #333;">Maliyetine Ev Sahibi Olmak Herkesin Hakkı</p>
+                <div class="elements-social social-icon-style-02">
+                    <ul class="small-icon light list-inline mb-0">
+                        @foreach($socialMediaIcons as $icon)
+                            <li><a class="facebook" href="{{$icon->url}}" target="_blank"><i class="fa-brands {{$icon->icon_class}}"></i></a></li>
                         @endforeach
                     </ul>
                 </div>
             </div>
-
-
-            <div class="col-12 col-lg-2 col-sm-4 xs-mb-30px order-sm-3 order-lg-2" style="width: 390px !important">
-                <span class="fs-18 fw-400 d-block mb-5px" style="color: #333">Hızlı Link</span>
+            <div class="col-12 col-lg-2 col-sm-4 mb-4 mb-lg-0" style="width: 390px;">
+                <span class="fs-16 fw-400 d-block mb-2" style="color: #333;">Hızlı Link</span>
                 <ul class="menu-list">
-                    @foreach ($menu as $menus)
-                        <li><a href="{{$menus->href}}" style="color: #333">{{$menus->text}}</a></li>
+                    @foreach ($menu as $item)
+                        <li><a href="{{$item->href}}" style="color: #333;">{{$item->text}}</a></li>
                     @endforeach
-                    <li><a href="/page/hakkimizda" style="color: #333">Hakkımızda</a></li>
-                    <li><a href="/musterilerimize_mektup" style="color: #333">Müşterilerimze Mektup</a></li>
+                    <li><a href="/page/hakkimizda" style="color: #333;">Hakkımızda</a></li>
+                    <li><a href="/musterilerimize_mektup" style="color: #333;">Müşterilerimizden Mektup</a></li>
                 </ul>
             </div>
-
-
-            <div class="col-12 col-lg-2 col-sm-4 xs-mb-30px order-sm-5 order-lg-4">
-                <span class="fs-18 fw-400 d-block mb-5px" style="color: #333">İletişim</span>
-                <a href="tel:{{$settings['phone']}}" style="color: #333;">{{$settings['phone']}}</a>
-                <a href="mailto:{{$settings['e-mail']}}" class="lh-16 d-block" style="color: #333">{{$settings['e-mail']}}</a>
+            <div class="col-12 col-lg-2 col-sm-4 mb-4 mb-lg-0">
+                <span class="fs-16 fw-400 d-block mb-2" style="color: #333;">İletişim</span>
+                <a href="tel:{{$settings['phone']}}" style="color: #333;">{{$settings['phone']}}</a><br>
+                <a href="mailto:{{$settings['e-mail']}}" style="color: #333;">{{$settings['e-mail']}}</a>
             </div>
-
-
-            <div class="col-lg-3 col-sm-12 md-mb-40px xs-mb-0 order-sm-2 order-lg-5">
-                <span class="fs-18 fw-400 d-block mb-5px" style="color: #333">Mail gönderin</span>
-                <p class="mb-20px" style="color: #333">En kısa sürede dönüş yapacağız.</p>
-                <div class="d-inline-block w-100 newsletter-style-02 position-relative">
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul class="mb-0">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    <form method="POST" action="{{ route('footer.form.show') }}" class="position-relative">
-                        @csrf
-                        <label for="">E-Mail Adresinizi Giriniz</label>
-
-                        <input
-                            class="border-color-transparent-white-light  border-radius-4px w-100 form-control lg-ps-15px required fs-16  input-placeholder-black"
-                            type="email" name="email" placeholder="Email Adresiniz" />
-                        <input type="hidden" name="redirect" value>
-                        <button class="btn pe-20px" aria-label="submit"><i
-                                class="fa fa-envelope icon-small " style="padding: 6px 5px 9px;"></i></button>
-                        <div
-                            class="form-results border-radius-4px pt-5px pb-5px ps-15px pe-15px fs-14 lh-22 mt-10px w-100 text-center position-absolute d-none">
-                        </div>
-                    </form>
-                </div>
+            <div class="col-lg-3 col-sm-12">
+                <span class="fs-16 fw-400 d-block mb-2" style="color: #333;">Mail gönderin</span>
+                <p style="color: #333;">En kısa sürede dönüş yapacağız.</p>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul class="mb-0">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <form method="POST" action="{{ route('footer.form.show') }}">
+                    @csrf
+                    <input class="form-control mb-2" type="email" name="email" placeholder="Email Adresiniz">
+                    <button class="btn btn-primary" type="submit" aria-label="Gönder"><i class="fa fa-envelope"></i></button>
+                </form>
             </div>
-
         </div>
+    </div>
 
+    <div class="container mt-4">
+        <div class="row justify-content-center fs-14 fw-300">
+            <div class="col-12 col-lg-3 mb-3 mb-lg-0">
+                <p style="color: #333;">&copy; {{ date('Y') }} Maliyetine Ev. Tüm hakları saklıdır.</p>
+            </div>
+        </div>
     </div>
 </footer>
 
