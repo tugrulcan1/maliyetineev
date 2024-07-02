ac50c6c2fb9665f9c129fa7b77b55ba9ccc78865
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-02 15:15:58 +0300

    "Updated footer layout and design, removed old code and added new styles and scripts."

--- resources/views/client/layouts/partials/footer.blade.php
+++ resources/views/client/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer-dark pt-3 pb-5" style="background-color: #dddddd;">
+{{-- <footer class="footer-dark pt-3 pb-5" style="background-color: #dddddd;">
     <div class="container">
         <div class="row align-items-center justify-content-center">
             <div class="col-xl-6 text-center text-xl-start mb-sm-0">
@@ -6,11 +6,13 @@
                 <p class="fs-15 fw-300" style="color: #333;">Bizimle iletişime geçebilirsiniz.</p>
             </div>
             <div class="col-xl-6 text-center text-xl-end">
-                <a href="mailto:{{$settings['e-mail']}}" class="btn btn-extra-large btn-yellow btn-rounded text-white me-2">
-                    <i class="fa fa-envelope"></i> {{$settings['e-mail']}}
+                <a href="mailto:{{ $settings['e-mail'] }}"
+                    class="btn btn-extra-large btn-yellow btn-rounded text-white me-2">
+                    <i class="fa fa-envelope"></i> {{ $settings['e-mail'] }}
                 </a>
-                <a href="tel:{{$settings['phone']}}" class="btn btn-extra-large btn-base-color btn-rounded text-white">
-                    <i class="fa fa-phone"></i> {{$settings['phone']}}
+                <a href="tel:{{ $settings['phone'] }}"
+                    class="btn btn-extra-large btn-base-color btn-rounded text-white">
+                    <i class="fa fa-phone"></i> {{ $settings['phone'] }}
                 </a>
             </div>
         </div>
@@ -25,8 +27,9 @@
                 <p class="fs-14" style="color: #333;">Maliyetine Ev Sahibi Olmak Herkesin Hakkı</p>
                 <div class="elements-social social-icon-style-02">
                     <ul class="small-icon light list-inline mb-0">
-                        @foreach($socialMediaIcons as $icon)
-                            <li><a class="facebook" href="{{$icon->url}}" target="_blank"><i class="fa-brands {{$icon->icon_class}}"></i></a></li>
+                        @foreach ($socialMediaIcons as $icon)
+                            <li><a class="facebook" href="{{ $icon->url }}" target="_blank"><i
+                                        class="fa-brands {{ $icon->icon_class }}"></i></a></li>
                         @endforeach
                     </ul>
                 </div>
@@ -41,27 +44,9 @@
             </div>
             <div class="col-12 col-lg-4 col-sm-4 mb-4 mb-lg-0">
                 <span class="fs-16 fw-400 d-block mb-2" style="color: #333;">İletişim</span>
-                <a href="tel:{{$settings['phone']}}" style="color: #333;">{{$settings['phone']}}</a><br>
-                <a href="mailto:{{$settings['e-mail']}}" style="color: #333;">{{$settings['e-mail']}}</a>
+                <a href="tel:{{ $settings['phone'] }}" style="color: #333;">{{ $settings['phone'] }}</a><br>
+                <a href="mailto:{{ $settings['e-mail'] }}" style="color: #333;">{{ $settings['e-mail'] }}</a>
             </div>
-            {{-- <div class="col-lg-3 col-sm-12">
-                <span class="fs-16 fw-400 d-block mb-2" style="color: #333;">Mail gönderin</span>
-                <p style="color: #333;">En kısa sürede dönüş yapacağız.</p>
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul class="mb-0">
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-                <form method="POST" action="{{ route('footer.form.show') }}">
-                    @csrf
-                    <input class="form-control mb-2" type="email" name="email" placeholder="Email Adresiniz">
-                    <button class="btn btn-primary" type="submit" aria-label="Gönder"><i class="fa fa-envelope"></i></button>
-                </form>
-            </div> --}}
         </div>
     </div>
 
@@ -72,56 +57,310 @@
             </div>
         </div>
     </div>
-</footer>
+</footer> --}}
+
+<section class="section footer-main" id="section_805038068">
+    <div class="bg section-bg fill bg-fill bg-loaded">
 
 
-<!-- <div class="sticky-wrap d-none d-lg-inline-block" data-animation-delay="100" data-shadow-animation="true">
-        <span class="fs-16"><i class="bi bi-envelope icon-small me-10px"></i>Get our service — <a
-                href="cdn-cgi/l/email-protection.html#751c1b131a35111a18141c1b5b161a18"
-                class="text-decoration-line-bottom fw-500">Send a message</a></span>
-    </div> -->
+
+
+
+    </div>
+
+
+
+    <div class="section-content relative">
+
+
+        <div class="row" id="row-1722533399">
+
+
+            <div id="col-1292317778" class="col medium-8 small-12 large-8">
+                <div class="col-inner">
+
+
+
+                    <div class="img has-hover x md-x lg-x y md-y lg-y" id="image_1517208972">
+                        <div class="img-inner dark">
+                            <img width="1020" height="181"
+                                src="{{ asset('images/maliyetineevlogo.png') }}"
+                                class="attachment-large size-large" alt="" decoding="async" loading="lazy"
+                                srcset="{{ asset('images/maliyetineevlogo.png') }}"
+                                sizes="(max-width: 1020px) 100vw, 1020px">
+                        </div>
+
+                        <style>
+                            #image_1517208972 {
+                                width: 34%;
+                            }
+                        </style>
+                    </div>
+
+
+                    <div id="text-4167961948" class="text">
+
+
+                        <p class="mb-0">Cevizli, Çanakkale Cd. No:103A, 34865 Kartal/İstanbul</p>
+
+                        <style>
+                            #text-4167961948 {
+                                line-height: 2.5;
+                                text-align: left;
+                                color: rgb(255, 255, 255);
+                            }
+
+                            #text-4167961948>* {
+                                color: rgb(255, 255, 255);
+                            }
+                        </style>
+                    </div>
+
+                    <div id="text-3587965853" class="text">
+
+
+                        <p class="mb-0"><a href="mailto:info@maliyetineev.com">info@maliyetineev.com</a></p>
+
+                        <style>
+                            #text-3587965853 {
+                                text-align: left;
+                                color: rgb(255, 255, 255);
+                            }
+
+                            #text-3587965853>* {
+                                color: rgb(255, 255, 255);
+                            }
+                        </style>
+                    </div>
+
+                    <div id="text-1301000021" class="text">
+
+
+                        <p><a href="tel:4443284">444 3 284</a></p>
+
+                        <style>
+                            #text-1301000021 {
+                                font-size: 1.85rem;
+                            }
+                        </style>
+                    </div>
+
+                    <div id="stack-2798831354"
+                        class="stack footer-menu stack-row justify-start items-stretch sm:stack-col sm:justify-between sm:items-center md:justify-between">
+
+                        <a href="/page/hakkimizda" class="button white is-link">
+                            <span>Hakkımızda</span>
+                        </a>
+                        <a href="/projelerimiz" class="button white is-link">
+                            <span>Projelerimiz</span>
+                        </a>
+
+
+
+
+
+                        <a href="/hakkimizda/" class="button white is-link">
+                            <span>Hakkımızda</span>
+                        </a>
+
+
+                        <a href="/subelerimiz/" class="button white is-link">
+                            <span>Şubelerimiz</span>
+                        </a>
+
+
+
+
+                        <style>
+                            #stack-2798831354>* {
+                                --stack-gap: 0rem;
+                            }
+
+                            @media (min-width:550px) {
+                                #stack-2798831354>* {
+                                    --stack-gap: 0rem;
+                                }
+                            }
+
+                            @media (min-width:850px) {
+                                #stack-2798831354>* {
+                                    --stack-gap: 2.5rem;
+                                }
+                            }
+                        </style>
+                    </div>
+
+                    <div class="social-icons follow-icons footer-social full-width text-left"><a
+                            href="https://www.facebook.com/EgeYapiGroup" target="_blank" data-label="Facebook"
+                            class="icon plain facebook tooltip" title="Facebook" aria-label="Facebook"
+                            rel="noopener nofollow"><i class="icon-facebook"></i></a><a
+                            href="https://www.instagram.com/egeyapi/" target="_blank" data-label="Instagram"
+                            class="icon plain instagram tooltip" title="Instagram" aria-label="Instagram"
+                            rel="noopener nofollow"><i class="icon-instagram"></i></a><a href="mailto:info@egeyapi.com"
+                            data-label="E-mail" target="_blank" class="icon plain email tooltip"
+                            title="Bize e-posta gönderin" aria-label="Bize e-posta gönderin" rel="nofollow noopener"><i
+                                class="icon-envelop"></i></a><a href="tel:4445343" data-label="Phone" target="_blank"
+                            class="icon plain phone tooltip" title="Bizi arayın" aria-label="Bizi arayın"
+                            rel="nofollow noopener"><i class="icon-phone"></i></a><a
+                            href="https://www.linkedin.com/company/egeyapigyo" data-label="LinkedIn" target="_blank"
+                            class="icon plain linkedin tooltip" title="Linkedin" aria-label="Linkedin"
+                            rel="noopener nofollow"><i class="icon-linkedin"></i></a></div>
+
+
+                </div>
+            </div>
+
+
+
+            <div id="col-96270997" class="col medium-4 small-12 large-4">
+                <div class="col-inner">
+
+
+
+                    <div id="text-817466606" class="text">
+
+
+                        <p class="mb-0">E-bülten'e Kayıt Olun</p>
+
+                        <style>
+                            #text-817466606 {
+                                font-size: 1.35rem;
+                                line-height: 2.75;
+                            }
+                        </style>
+                    </div>
+
+                    <p>E-bültenimize abone olabilirsiniz</p>
+
+                    <div class="wpcf7 js" id="wpcf7-f1007-o1" lang="tr-TR" dir="ltr">
+                        <div class="screen-reader-response">
+                            <p role="status" aria-live="polite" aria-atomic="true"></p>
+                            <ul></ul>
+                        </div>
+                        <form action="/#wpcf7-f1007-o1" method="post" class="wpcf7-form init"
+                            aria-label="İletişim Formu" novalidate="novalidate" data-status="init">
+                            <div style="display: none;">
+                                <input type="hidden" name="_wpcf7" value="1007">
+                                <input type="hidden" name="_wpcf7_version" value="5.9.6">
+                                <input type="hidden" name="_wpcf7_locale" value="tr_TR">
+                                <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1007-o1">
+                                <input type="hidden" name="_wpcf7_container_post" value="0">
+                                <input type="hidden" name="_wpcf7_posted_data_hash" value="">
+                            </div>
+                            <div class="inputs">
+                                <div class="input">
+                                    <p><label> E-posta</label><br>
+                                        <span class="wpcf7-form-control-wrap" data-name="EmailAdress"><input
+                                                size="40" maxlength="80"
+                                                class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                                                aria-required="true" aria-invalid="false" value=""
+                                                type="email" name="EmailAdress"></span>
+                                    </p>
+                                    <p><input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit"
+                                            value="Gönder" disabled=""><span class="wpcf7-spinner"></span>
+                                    </p>
+                                </div>
+                                <p><span class="wpcf7-form-control-wrap" data-name="kvkkizin"><span
+                                            class="wpcf7-form-control wpcf7-acceptance"><span
+                                                class="wpcf7-list-item"><label><input type="checkbox" name="kvkkizin"
+                                                        value="1" aria-invalid="false"><span
+                                                        class="wpcf7-list-item-label"><a href="javascript:;"
+                                                            data-pikap-popup="popup-kvkk">Kişisel Verilerin Korunması
+                                                            Kanunu kapsamında gerçekleştirilen bilgilendirmeyi okudum,
+                                                            onaylıyorum.</a></span></label></span></span></span>
+                                </p>
+                            </div>
+                            <div class="wpcf7-response-output" aria-hidden="true"></div>
+                        </form>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+
+
+        </div>
+        <div class="row" id="row-464371575">
+
+
+            <div id="col-283855390" class="col pb-0 footer-copy small-12 large-12">
+                <div class="col-inner">
+
+
+
+                    <div class="is-divider divider clearfix"
+                        style="margin-top:1.6em;margin-bottom:1.6em;max-width:100%;height:1px;background-color:rgba(255, 255, 255, 0.233);">
+                    </div>
+
+                    <div id="text-1185031307" class="text">
+
+
+                        <p class="mb-0">© 2024 Maliyetine Ev | Tüm Hakları Saklıdır. <a style="margin-left: 30px;"
+                                href="javascript:;" data-pikap-popup="popup-kvkk">Kişisel Verilerin Korunması</a><a
+                                style="margin-left: 30px;" href="javascript:;" data-pikap-popup="popup-ticari">Ticari
+                                Elektronik İleti Açık Rıza Metni</a></p>
+
+
+                        <style>
+                            #text-1185031307 {
+                                text-align: left;
+                            }
+                        </style>
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </div>
+
 
     <style>
-        .menu-list {
-    display: flex;
-    flex-wrap: wrap;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-}
-
-.menu-list li {
-    flex: 0 0 50%; /* Her bir öğenin genişliği %50 olacak şekilde ayarlıyoruz */
-    margin-bottom: 10px; /* Öğeler arasına biraz boşluk ekliyoruz */
-}
-
-.menu-list li a {
-    color: white; /* Bağlantı metin rengini beyaz yapıyoruz */
-    text-decoration: none; /* Bağlantı alt çizgilerini kaldırıyoruz */
-}
-
-.menu-list li a:hover {
-    text-decoration: underline; /* Bağlantıların üzerine gelindiğinde alt çizgi ekliyoruz */
-}
-
+        #section_805038068 {
+            padding-top: 70px;
+            padding-bottom: 70px;
+            background-color: rgb(0, 96, 162);
+        }
     </style>
+</section>
 
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $(".tablinks").click(function(){
-                var tabId = $(this).data("tab");
-                console.log('adfsa'+tabId)
-                $(".tabcontent").hide();
-                $("#" + tabId).show();
-                $(".tablinks").removeClass("active");
-                $(this).addClass("active");
-            });
-            // Automatically click the first tab button on page load
-            $(".tab button:first").click();
+<style>
+    .menu-list li a {
+        color: white;
+        /* Bağlantı metin rengini beyaz yapıyoruz */
+        text-decoration: none;
+        /* Bağlantı alt çizgilerini kaldırıyoruz */
+    }
+
+    .menu-list li a:hover {
+        text-decoration: underline;
+        /* Bağlantıların üzerine gelindiğinde alt çizgi ekliyoruz */
+    }
+</style>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $(".tablinks").click(function() {
+            var tabId = $(this).data("tab");
+            console.log('adfsa' + tabId)
+            $(".tabcontent").hide();
+            $("#" + tabId).show();
+            $(".tablinks").removeClass("active");
+            $(this).addClass("active");
         });
-    </script>
+        // Automatically click the first tab button on page load
+        $(".tab button:first").click();
+    });
+</script>
 <script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
 <script type="text/javascript" src="{{ URL::to('/') }}/js/jquery.js"></script>
 <script type="text/javascript" src="{{ URL::to('/') }}/js/vendors.min.js"></script>
@@ -147,25 +386,25 @@
     }
 
     function openCity(evt, cityName) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-                tabcontent[i].style.display = "none";
-            }
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-            document.getElementById(cityName).style.display = "block";
-            if (evt) {
-                evt.currentTarget.className += " active";
-            }
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
         }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        if (evt) {
+            evt.currentTarget.className += " active";
+        }
+    }
 
-        // Sayfa yüklendiğinde ilk sekmeyi aç
-        document.addEventListener("DOMContentLoaded", function () {
-            document.querySelector(".tablinks").click();
-        });
+    // Sayfa yüklendiğinde ilk sekmeyi aç
+    document.addEventListener("DOMContentLoaded", function() {
+        document.querySelector(".tablinks").click();
+    });
 </script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 {{-- <script>
