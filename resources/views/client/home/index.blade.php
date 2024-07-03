0e3996024c5bc3d2209c140d4ab3c5d73ff96d6a
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-03 17:29:54 +0300

    update

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -145,11 +145,11 @@
          </div>
      </section>
 
-<section class="p-0 bg-base-color">
-<div class="container">
+<section class="p-0 ">
+<div class="container bg-base-color">
 <div class="row align-items-center justify-content-center g-0">
 <div class="col-auto d-flex align-items-center appear anime-complete" data-anime="{ &quot;translateY&quot;: [0, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 1200, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 150, &quot;easing&quot;: &quot;easeOutQuad&quot; }" style="">
-<img src="/satisi-devam-eden" alt="" data-no-retina="">
+<img src="{{asset('favicon-c9c0de2311390de484c5d988188059cd56ed9317.png')}}" alt="" data-no-retina="">
 <div class="fs-19 lh-28 last-paragraph-no-margin text-white pt-15px pb-15px">
 <p>MALİYETİNE EV SİZLERE %100 GÜVEN VE
                             GARANTİ SUNUYOR! <a href="/iletisim" class="text-decoration-line-bottom fw-500 text-white">Başvuru Yap</a></p>
