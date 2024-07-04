97e5ecd2aa157397c84aa4c4ddf0f83f6810ed77
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-04 11:13:53 +0300

    update

--- resources/views/client/basinda_biz.blade.php
+++ resources/views/client/basinda_biz.blade.php
@@ -1,34 +1,17 @@
 @extends('client.layouts.master')
 
 @section('content')
+
     <section class="page-title-big-typography bg-dark-gray ipad-top-space-margin" data-parallax-background-ratio="0.5"
-        style="background-image: url(&quot;images/demo-conference-page-bg.jpg&quot;); margin-top: inherit; background-position: 50% 0px;">
+        style="background-image: url(images/demo-conference-page-bg.jpg)">
         <div class="container">
             <div class="row align-items-center justify-content-center extra-small-screen">
                 <div class="col-lg-8 text-center page-title-extra-large">
                     <h1 class="mb-0 text-white alt-font fw-600 ls-minus-5px">
-                        <span class="d-block text-outline text-outline-width-2px text-outline-color-white appear"
-                        data-fancy-text="{ &quot;string&quot;: [&quot;Event&quot;], &quot;duration&quot;: 500, &quot;delay&quot;: 0, &quot;speed&quot;: 50, &quot;clipPath&quot;: [&quot;inset(0 500px 0 0)&quot;, &quot;inset(0px -5px 0px 0px)&quot;], &quot;easing&quot;: &quot;easeOutCubic&quot; }"><span
-                            class="anime-text words chars splitting" data-splitting="true"
-                            style="--word-total: 1; --char-total: 7;"><span class="word" data-word="basında"
-                                style="--word-index: 0;"><span class="char" data-char="b"
-                                style="--char-index: 0; clip-path: inset(0px -5px 0px 0px);">b</span><span
-                                class="char" data-char="a"
-                                style="--char-index: 1; clip-path: inset(0px -5px 0px 0px);">a</span><span
-                                class="char" data-char="s"
-                                style="--char-index: 2; clip-path: inset(0px -5px 0px 0px);">s</span><span
-                                class="char" data-char="ı"
-                                style="--char-index: 3; clip-path: inset(0px -5px 0px 0px);">ı</span><span
-                                class="char" data-char="n"
-                                style="--char-index: 4; clip-path: inset(0px -5px 0px 0px);">n</span><span
-                                class="char" data-char="d"
-                                style="--char-index: 5; clip-path: inset(0px -5px 0px 0px);">d</span><span
-                                class="char" data-char="a"
-                                style="--char-index: 6; clip-path: inset(0px -5px 0px 0px);">a</span></span></span></span>
-
+                        <span class="d-block text-outline text-outline-width-2px text-outline-color-white"
+                            data-fancy-text="{ &quot;string&quot;: [&quot;Basında&quot;], &quot;duration&quot;: 500, &quot;delay&quot;: 0, &quot;speed&quot;: 50, &quot;clipPath&quot;: [&quot;inset(0 500px 0 0)&quot;, &quot;inset(0px -5px 0px 0px)&quot;], &quot;easing&quot;: &quot;easeOutCubic&quot; }"></span>
                         <span
-                            data-anime="{ &quot;opacity&quot;: [0, 1], &quot;easing&quot;: &quot;easeOutQuad&quot;, &quot;duration&quot;: 1000, &quot;delay&quot;: 300 }"
-                            class="appear anime-complete" style="">biz</span>
+                            data-anime="{ &quot;opacity&quot;: [0, 1], &quot;easing&quot;: &quot;easeOutQuad&quot;, &quot;duration&quot;: 1000, &quot;delay&quot;: 300 }">Biz</span>
                     </h1>
                 </div>
             </div>
