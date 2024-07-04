654c8cfc20a8f4a8ac822f77bf8ea51e5ea67232
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-04 16:01:00 +0300

    update

--- resources/views/client/contact.blade.php
+++ resources/views/client/contact.blade.php
@@ -15,113 +15,126 @@
         </div>
     </section>
 
-    <div class="row justify-content-center align-items-center mb-9 sm-mb-45px">
-        <div class="col-xxl-5 col-lg-6 md-mb-50px appear anime-child anime-complete"
-        data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;translateX&quot;: [-50, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 1200, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 150, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
-        <span class="fs-15 text-uppercase text-base-color fw-600 mb-15px d-block ls-1px" style="">Bizimle iletişime geçin</span>
-        <h3 class="fw-700 text-dark-gray ls-minus-1px mb-50px sm-mb-35px" style="">Yardıma mı ihtiyacınız var? Şimdi bizimle iletişime geçin!</h3>
+    <section>
+        <div class="container">
+            <div class="row justify-content-center align-items-center mb-9 sm-mb-45px">
+                <div class="col-xxl-5 col-lg-6 md-mb-50px appear anime-child anime-complete"
+                    data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;translateX&quot;: [-50, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 1200, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 150, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
+                    <span class="fs-15 text-uppercase text-base-color fw-600 mb-15px d-block ls-1px" style="">Bizimle
+                        iletişime geçin</span>
+                    <h3 class="fw-700 text-dark-gray ls-minus-1px mb-50px sm-mb-35px" style="">Yardıma mı ihtiyacınız var?
+                        Şimdi bizimle iletişime geçin!</h3>
 
-        <div class="icon-with-text-style-01 mb-10 md-mb-35px" style="">
-            <div class="feature-box feature-box-left-icon last-paragraph-no-margin">
-                <div class="feature-box-icon me-25px">
-                    <img src="{{asset('demo-marketing-contact-04.jpg')}}" class="h-80px" alt="" data-no-retina="">
+                    <div class="icon-with-text-style-01 mb-10 md-mb-35px" style="">
+                        <div class="feature-box feature-box-left-icon last-paragraph-no-margin">
+                            <div class="feature-box-icon me-25px">
+                                <img src="{{ asset('demo-marketing-contact-04.jpg') }}" class="h-80px" alt=""
+                                    data-no-retina="">
+                            </div>
+                            <div class="feature-box-content last-paragraph-no-margin">
+                                <span class="d-block text-dark-gray fw-600 fs-18 ls-minus-05px mb-5px">Tanışmak için ofisimize
+                                    bekleriz</span>
+                                <p class="w-60 md-w-100">Cevizli, Çanakkale Cd. No:103A, 34865 Kartal/İstanbul</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="icon-with-text-style-01 mb-10 md-mb-35px" style="">
+                        <div class="feature-box feature-box-left-icon last-paragraph-no-margin">
+                            <div class="feature-box-icon me-25px">
+                                <img src="{{ asset('demo-marketing-contact-02.jpg') }}" class="h-80px" alt=""
+                                    data-no-retina="">
+                            </div>
+                            <div class="feature-box-content">
+                                <span class="d-block text-dark-gray fw-600 fs-18 ls-minus-05px mb-5px">Bizimle iletişime geçmekten
+                                    çekinmeyin</span>
+                                <div class="w-100 d-block">
+                                    <span class="d-block">Telefon: <a href="tel:4443284"> 444 3 284
+                                        </a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="icon-with-text-style-01" style="">
+                        <div class="feature-box feature-box-left-icon last-paragraph-no-margin">
+                            <div class="feature-box-icon me-25px">
+                                <img src="{{ asset('demo-marketing-contact-03.jpg') }}" class="h-80px" alt=""
+                                    data-no-retina="">
+                            </div>
+                            <div class="feature-box-content">
+                                <span class="d-block text-dark-gray fw-600 fs-18 ls-minus-05px mb-5px">Size nasıl yardımcı
+                                    olabiliriz?</span>
+                                <div class="w-100 d-block">
+                                    <a href="mailto: info@maliyetineev.com"> info@maliyetineev.com</a><br>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <div class="feature-box-content last-paragraph-no-margin">
-                    <span class="d-block text-dark-gray fw-600 fs-18 ls-minus-05px mb-5px">Tanışmak için ofisimize bekleriz</span>
-                    <p class="w-60 md-w-100">Cevizli, Çanakkale Cd. No:103A, 34865 Kartal/İstanbul</p>
+                <div class="col-lg-6 offset-xxl-1 appear anime-child anime-complete"
+                    data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;translateX&quot;: [50, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 1200, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 150, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
+                    <div class="contact-form-style-03 position-relative border-radius-10px bg-white p-14 lg-p-10 box-shadow-double-large overflow-hidden last-paragraph-no-margin"
+                        style="">
+                        <h2 class="fw-700 text-dark-gray mb-30px sm-mb-20px fancy-text-style-4 ls-minus-2px">Say
+                            <span
+                                data-fancy-text="{ &quot;effect&quot;: &quot;rotate&quot;, &quot;string&quot;: [&quot;hello!&quot;, &quot;hallå!&quot;, &quot;salve!&quot;] }"
+                                class="appear"><span class="anime-text words chars splitting" data-splitting="true"
+                                    style="--word-total: 1; --char-total: 6;"><span class="word" data-word="salve!"
+                                        style="--word-index: 0;"><span class="char" data-char="s"
+                                            style="--char-index: 0; opacity: 1; transform: rotateX(0deg);">s</span><span
+                                            class="char" data-char="a"
+                                            style="--char-index: 1; opacity: 1; transform: rotateX(0deg);">a</span><span
+                                            class="char" data-char="l"
+                                            style="--char-index: 2; opacity: 1; transform: rotateX(0deg);">l</span><span
+                                            class="char" data-char="v"
+                                            style="--char-index: 3; opacity: 1; transform: rotateX(0deg);">v</span><span
+                                            class="char" data-char="e"
+                                            style="--char-index: 4; opacity: 1; transform: rotateX(0deg);">e</span><span
+                                            class="char" data-char="!"
+                                            style="--char-index: 5; opacity: 1; transform: rotateX(0deg);">!</span></span></span></span>
+                        </h2>
+                        <form action="{{ route('contact.form.show') }}" method="POST" class="row contact-form-style-02">
+                            @csrf
+                            <div class="col-md-12 mb-30px">
+                                <input class="input-name form-control required" type="text" name="name"
+                                    placeholder="Ad soyad" />
+                            </div>
+                            <div class="col-md-12 mb-30px">
+                                <input class="form-control required" type="email" name="email" placeholder="E-mail adresiniz" />
+                            </div>
+                            <div class="col-md-12 mb-30px">
+                                <input class="form-control" type="tel" name="phone" placeholder="Telefon" />
+                            </div>
+                            <div class="col-md-12 mb-30px">
+                                <input class="form-control" type="text" name="job" placeholder="Mesleğiniz" />
+                            </div>
+                            <div class="col-md-12 mb-30px">
+                                <textarea class="form-control" cols="40" rows="4" name="message" placeholder="Mesajınız"></textarea>
+                            </div>
+                            <div class="col-xl-7 col-md-7 last-paragraph-no-margin">
+                                <p class="text-center text-md-start fs-15 lh-26">24 saat içerisinde dönüş sağlayacağız.
+                                </p>
+                            </div>
+                            <div class="col-xl-5 col-md-5 text-center text-md-end sm-mt-20px">
+                                <input type="hidden" name="redirect" value>
+                                <button
+                                    class="btn btn-base-color btn-medium btn-rounded btn-box-shadow"style="background: #0f75bc !important"
+                                    type="submit">Gönder</button>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-results mt-20px d-none"></div>
+                            </div>
+                        </form>
+                        <div class="position-absolute bottom-0px right-minus-30px fs-350 lh-100 fw-900 text-yellow">&lt;</div>
+                    </div>
                 </div>
             </div>
         </div>
+    </section>
 
-        <div class="icon-with-text-style-01 mb-10 md-mb-35px" style="">
-            <div class="feature-box feature-box-left-icon last-paragraph-no-margin">
-                <div class="feature-box-icon me-25px">
-                    <img src="{{asset('demo-marketing-contact-02.jpg')}}" class="h-80px" alt="" data-no-retina="">
-                </div>
-                <div class="feature-box-content">
-                    <span class="d-block text-dark-gray fw-600 fs-18 ls-minus-05px mb-5px">Bizimle iletişime geçmekten çekinmeyin</span>
-                    <div class="w-100 d-block">
-                        <span class="d-block">Telefon: <a href="tel:4443284"> 444 3 284
-                        </a></span>
-                    </div>
-                </div>
-            </div>
-        </div>
 
-        <div class="icon-with-text-style-01" style="">
-            <div class="feature-box feature-box-left-icon last-paragraph-no-margin">
-                <div class="feature-box-icon me-25px">
-                    <img src="{{asset('demo-marketing-contact-03.jpg')}}" class="h-80px" alt="" data-no-retina="">
-                </div>
-                <div class="feature-box-content">
-                    <span class="d-block text-dark-gray fw-600 fs-18 ls-minus-05px mb-5px">Size nasıl yardımcı olabiliriz?</span>
-                    <div class="w-100 d-block">
-                        <a href="mailto: info@maliyetineev.com"> info@maliyetineev.com</a><br>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-        <div class="col-lg-6 offset-xxl-1 appear anime-child anime-complete"
-            data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;translateX&quot;: [50, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 1200, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 150, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
-            <div class="contact-form-style-03 position-relative border-radius-10px bg-white p-14 lg-p-10 box-shadow-double-large overflow-hidden last-paragraph-no-margin"
-                style="">
-                <h2 class="fw-700 text-dark-gray mb-30px sm-mb-20px fancy-text-style-4 ls-minus-2px">Say
-                    <span
-                        data-fancy-text="{ &quot;effect&quot;: &quot;rotate&quot;, &quot;string&quot;: [&quot;hello!&quot;, &quot;hallå!&quot;, &quot;salve!&quot;] }"
-                        class="appear"><span class="anime-text words chars splitting" data-splitting="true"
-                            style="--word-total: 1; --char-total: 6;"><span class="word" data-word="salve!"
-                                style="--word-index: 0;"><span class="char" data-char="s"
-                                    style="--char-index: 0; opacity: 1; transform: rotateX(0deg);">s</span><span
-                                    class="char" data-char="a"
-                                    style="--char-index: 1; opacity: 1; transform: rotateX(0deg);">a</span><span
-                                    class="char" data-char="l"
-                                    style="--char-index: 2; opacity: 1; transform: rotateX(0deg);">l</span><span
-                                    class="char" data-char="v"
-                                    style="--char-index: 3; opacity: 1; transform: rotateX(0deg);">v</span><span
-                                    class="char" data-char="e"
-                                    style="--char-index: 4; opacity: 1; transform: rotateX(0deg);">e</span><span
-                                    class="char" data-char="!"
-                                    style="--char-index: 5; opacity: 1; transform: rotateX(0deg);">!</span></span></span></span>
-                </h2>
-                <form action="{{ route('contact.form.show') }}" method="POST" class="row contact-form-style-02">
-                    @csrf
-                    <div class="col-md-12 mb-30px">
-                        <input class="input-name form-control required" type="text" name="name"
-                            placeholder="Ad soyad" />
-                    </div>
-                    <div class="col-md-12 mb-30px">
-                        <input class="form-control required" type="email" name="email"
-                            placeholder="E-mail adresiniz" />
-                    </div>
-                    <div class="col-md-12 mb-30px">
-                        <input class="form-control" type="tel" name="phone" placeholder="Telefon" />
-                    </div>
-                    <div class="col-md-12 mb-30px">
-                        <input class="form-control" type="text" name="job" placeholder="Mesleğiniz" />
-                    </div>
-                    <div class="col-md-12 mb-30px">
-                        <textarea class="form-control" cols="40" rows="4" name="message" placeholder="Mesajınız"></textarea>
-                    </div>
-                    <div class="col-xl-7 col-md-7 last-paragraph-no-margin">
-                        <p class="text-center text-md-start fs-15 lh-26">24 saat içerisinde dönüş sağlayacağız.
-                        </p>
-                    </div>
-                    <div class="col-xl-5 col-md-5 text-center text-md-end sm-mt-20px">
-                        <input type="hidden" name="redirect" value>
-                        <button
-                            class="btn btn-base-color btn-medium btn-rounded btn-box-shadow"style="background: #0f75bc !important"
-                            type="submit">Gönder</button>
-                    </div>
-                    <div class="col-12">
-                        <div class="form-results mt-20px d-none"></div>
-                    </div>
-                </form>
-                <div class="position-absolute bottom-0px right-minus-30px fs-350 lh-100 fw-900 text-yellow">&lt;</div>
-            </div>
-        </div>
-    </div>
 
 
     <section class="top-space-margin page-title-big-typography cover-background magic-cursor round-cursor"
@@ -129,12 +142,12 @@
         <div class="container">
             <div class="row extra-very-small-screen align-items-center">
                 <!-- <div class="col-lg-5 col-sm-8 position-relative page-title-extra-small"
-                                                    data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;opacity&quot;: [0, 1], &quot;translateX&quot;: [-30, 0], &quot;duration&quot;: 800, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
-                                                    <h1 class="mb-20px xs-mb-20px text-white text-shadow-medium"><span
-                                                            class="w-30px h-2px bg-yellow d-inline-block align-middle position-relative top-minus-2px me-10px"></span>Contact
-                                                        us</h1>
-                                                    <h2 class="text-white text-shadow-medium fw-500 ls-minus-2px mb-0">Clients relationships</h2>
-                                                </div> -->
+                                                        data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;opacity&quot;: [0, 1], &quot;translateX&quot;: [-30, 0], &quot;duration&quot;: 800, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
+                                                        <h1 class="mb-20px xs-mb-20px text-white text-shadow-medium"><span
+                                                                class="w-30px h-2px bg-yellow d-inline-block align-middle position-relative top-minus-2px me-10px"></span>Contact
+                                                            us</h1>
+                                                        <h2 class="text-white text-shadow-medium fw-500 ls-minus-2px mb-0">Clients relationships</h2>
+                                                    </div> -->
             </div>
         </div>
     </section>
