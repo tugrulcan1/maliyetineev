c71c801aa869e0683d6e1b3a6083797e6c63ad6c
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-05 11:36:23 +0300

    update

--- resources/views/client/whatIsPage.blade.php
+++ resources/views/client/whatIsPage.blade.php
@@ -20,28 +20,45 @@ padding-bottom: 30px;">
 
     <section>
         <div class="container">
+            <div class="bg-solitude-blue border-radius-6px p-45px lg-p-30px mb-25px" style="">
+                <span class="fs-19 alt-font text-dark-gray fw-700 mb-20px d-inline-block">Corporate services</span>
+                <ul class="p-0 m-0 list-style-02 fw-500">
+                    <li class="pb-10px mb-10px border-bottom border-color-transparent-dark"><a id="v-pills-home-tab"
+                            data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab"
+                            aria-controls="v-pills-home">Business planing</a><i
+                            class="fa fa-arrow-right ms-auto"></i></li>
+                    <li class="pb-10px mb-10px border-bottom border-color-transparent-dark text-dark-gray"><a
+                            id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button"
+                            role="tab" aria-controls="v-pills-profile" aria-selected="false"
+                            class="text-dark-gray text-dark-gray-hover">Market
+                            research</a><i class="fa fa-arrow-right ms-auto"></i></li>
+                    <li class="pb-10px mb-10px border-bottom border-color-transparent-dark"><a id="v-pills-messages-tab"
+                            data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab"
+                            aria-controls="v-pills-messages" aria-selected="false">Business consulting</a><i
+                            class="fa fa-arrow-right ms-auto"></i></li>
+                    <li class="pb-10px mb-10px border-bottom border-color-transparent-dark"><a id="v-pills-settings-tab"
+                            data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab"
+                            aria-controls="v-pills-settings" aria-selected="false">Audience analysis</a><i
+                            class="fa fa-arrow-right ms-auto"></i></li>
+                    <li><a href="demo-corporate-services-details.html">Business research</a><i
+                            class="fa fa-arrow-right ms-auto"></i></li>
+                </ul>
+            </div>
             <div class="d-flex align-items-start">
-                <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home"
-                        type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
-                    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile"
-                        type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
-                    <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages"
-                        type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
-                    <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings"
-                        type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
-                </div>
                 <div class="tab-content" id="v-pills-tabContent">
-                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel"
+                        aria-labelledby="v-pills-home-tab">...
                     </div>
-                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...
+                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                        ...</div>
+                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                        ...
                     </div>
-                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...
+                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                        ...
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
 @endsection
