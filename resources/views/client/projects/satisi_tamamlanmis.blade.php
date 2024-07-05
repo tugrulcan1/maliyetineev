e9501a079a449b77775772531c9ada63fe6235d7
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-05 14:15:32 +0300

    update

--- resources/views/client/projects/satisi_tamamlanmis.blade.php
+++ resources/views/client/projects/satisi_tamamlanmis.blade.php
@@ -24,7 +24,7 @@
                 <div class="col-lg-8 text-center page-title-extra-large">
                     <h1 class="mb-0 text-white alt-font fw-600 ls-minus-5px">
                         <span class="d-block text-outline text-outline-width-2px text-outline-color-white"
-                            data-fancy-text="{ &quot;string&quot;: [&quot;Tamamlanan Projeler&quot;], &quot;duration&quot;: 500, &quot;delay&quot;: 0, &quot;speed&quot;: 50, &quot;clipPath&quot;: [&quot;inset(0 500px 0 0)&quot;, &quot;inset(0px -5px 0px 0px)&quot;], &quot;easing&quot;: &quot;easeOutCubic&quot; }"></span>
+                            data-fancy-text="{ &quot;string&quot;: [&quot;Tamamlanan&quot;], &quot;duration&quot;: 500, &quot;delay&quot;: 0, &quot;speed&quot;: 50, &quot;clipPath&quot;: [&quot;inset(0 500px 0 0)&quot;, &quot;inset(0px -5px 0px 0px)&quot;], &quot;easing&quot;: &quot;easeOutCubic&quot; }"></span>
                         <span
                             data-anime="{ &quot;opacity&quot;: [0, 1], &quot;easing&quot;: &quot;easeOutQuad&quot;, &quot;duration&quot;: 1000, &quot;delay&quot;: 300 }">PROJELERİMİZ</span>
                     </h1>
