4061ab4e4c68565440180e253283fecd8e9c1ae9
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-07-05 13:27:54 +0300

    update

--- resources/views/client/layouts/partials/footer.blade.php
+++ resources/views/client/layouts/partials/footer.blade.php
@@ -119,7 +119,7 @@
 <script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
 <script type="text/javascript" src="{{ URL::to('/') }}/js/jquery.js"></script>
 <script type="text/javascript" src="{{ URL::to('/') }}/js/vendors.min.js"></script>
-<script type="text/javascript" src="{{ URL::to('/') }}/js/main.js"></script>
+{{-- <script type="text/javascript" src="{{ URL::to('/') }}/js/main.js"></script> --}}
 
 @stack('scripts')
 <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
