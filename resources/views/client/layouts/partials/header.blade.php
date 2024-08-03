9edced81f7b898e81162ba436f9d3e932fa96815
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-08-03 16:43:32 +0300

    "Updated button style in header.blade.php"

--- resources/views/client/layouts/partials/header.blade.php
+++ resources/views/client/layouts/partials/header.blade.php
@@ -659,7 +659,7 @@
                                     class="widget-text text-black text-black-hover">444 3 284</a></div>
                         </div>
                         <div class="header-button"><a href="https://emlaksepette.com/magaza/maliyetine-ev/106"
-                                target="_blank" style="border-color: #EC2F2E !important;color: #EC2F2E !important"
+                                target="_blank" style="border-color: #EC2F2E !important;background-color: #EC2F2E !important;color:White"
                                 class="btn btn-medium btn-transparent-white text-black border-1 border-color-black btn-rounded left-icon">Dijital
                                 Satış Ofisi</a></div>
                     </div>
