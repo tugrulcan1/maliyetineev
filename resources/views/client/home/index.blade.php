941f50585afb2cc817ad65adfd0efaff79ced4f6
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-08-03 16:52:54 +0300

    "Updated index.blade.php: Replaced bi icons with fa icons, added new section with client feedback and reviews"

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -46,7 +46,7 @@
                              <span class="text-white opacity-8 fw-500 d-block mb-5px">24 Saatte Tapu Teslim</span>
                              <h5 class="mb-0 fw-700 text-white">ÖNCE TAPUNUZU ALIN SONRA PEŞİNATINIZI ÖDEYİN</h5>
                              <div class="position-absolute top-0 end-0">
-                                 <img src="{{asset('demo-logistics-diagonal-line-01.svg')}}" alt="" height="100"
+                                 <img src="{{ asset('demo-logistics-diagonal-line-01.svg') }}" alt="" height="100"
                                      width="100" data-no-retina="">
                              </div>
                          </div>
@@ -165,11 +165,11 @@
                      <div class="d-flex justify-content-center justify-content-lg-start">
                          <div
                              class="slider-one-slide-prev-1 swiper-button-prev slider-navigation-style-04 bg-blue-licorice">
-                             <i class="bi bi-arrow-left-short icon-very-medium text-white"></i>
+                             <i class="fa fa-arrow-left-short icon-very-medium text-white"></i>
                          </div>
                          <div
                              class="slider-one-slide-next-1 swiper-button-next slider-navigation-style-04 bg-blue-licorice">
-                             <i class="bi bi-arrow-right-short icon-very-medium text-white"></i>
+                             <i class="fa fa-arrow-right-short icon-very-medium text-white"></i>
                          </div>
                      </div>
                  </div>
@@ -204,7 +204,7 @@
                                                          <a href="{{ route('projelerimiz.show', $project->id) }}"
                                                              class="circle-box bg-white w-55px h-55px rounded-circle ms-auto position-relative rounded-box">
                                                              <i
-                                                                 class="bi bi-arrow-right-short absolute-middle-center icon-very-medium lh-0px text-dark-gray"></i>
+                                                                 class="fa fa-arrow-right-short absolute-middle-center icon-very-medium lh-0px text-dark-gray"></i>
                                                          </a>
                                                      </div>
                                                      <div
@@ -296,6 +296,122 @@
 
 
 
+     <section class="position-relative overflow-hidden">
+         <img src="images/demo-real-estate-07.png" alt class="position-absolute left-100px top-50px z-index-minus-1"
+             data-bottom-top="transform: translate3d(80px, 0px, 0px);"
+             data-top-bottom="transform: translate3d(-180px, 0px, 0px);" />
+         <img src="images/demo-real-estate-08.png" alt class="position-absolute right-100px top-100px z-index-minus-1"
+             data-bottom-top="transform:scale(1.4, 1.4) translate3d(0px, 0px, 0px);"
+             data-top-bottom="transform:scale(1, 1) translate3d(-400px, 0px, 0px);" />
+         <div class="container">
+             <div class="row align-items-center justify-content-center">
+                 <div class="col-lg-6 text-end md-mb-50px"
+                     data-anime="{ &quot;effect&quot;: &quot;slide&quot;, &quot;color&quot;: &quot;#262b35&quot;, &quot;direction&quot;:&quot;lr&quot;, &quot;easing&quot;: &quot;easeOutQuad&quot;, &quot;delay&quot;:50}">
+                     <figure class="position-relative m-0">
+                         <img src="images/demo-real-estate-09.jpg" class="w-90 border-radius-6px" alt>
+                         <figcaption
+                             class="position-absolute bg-dark-gray border-radius-10px box-shadow-quadruple-large bottom-100px xs-bottom-minus-20px left-minus-30px md-left-0px w-230px xs-w-210px text-center last-paragraph-no-margin animation-float">
+                             <div
+                                 class="bg-white pt-35px pb-35px border-radius-8px mb-15px position-relative top-minus-1px">
+                                 <h1 class="fw-700 ls-minus-3px text-dark-gray mb-0 alt-font">4.8</h1>
+                                 <div class="text-golden-yellow fs-18 ls-1px">
+                                     <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
+                                         class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
+                                         class="fa fa-star-fill"></i>
+                                 </div>
+                                 <span class="text-dark-gray d-block alt-font fw-600">2488 Reviews</span>
+                                 <div
+                                     class="d-inline-block fs-12 text-uppercase bg-base-color ps-20px pe-20px lh-30 text-white border-radius-100px box-shadow-large">
+                                     Excellent score</div>
+                             </div>
+                             <img src="images/demo-real-estate-trustpilot.svg" class="h-30px mb-20px" alt />
+                         </figcaption>
+                     </figure>
+                 </div>
+                 <div class="col-lg-5 offset-lg-1 col-md-10 text-center text-lg-start"
+                     data-anime="{ &quot;translateY&quot;: [0, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 1200, &quot;delay&quot;: 100, &quot;staggervalue&quot;: 150, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
+                     <span class="fs-20 d-inline-block mb-15px text-base-color">Clients feedback</span>
+                     <h2 class="alt-font fw-500 text-dark-gray ls-minus-1px shadow-none" data-shadow-animation="true"
+                         data-animation-delay="700">Here is what our <span
+                             class="fw-700 text-highlight d-inline-block">client's<span
+                                 class="bg-base-color h-10px bottom-4px opacity-3 separator-animation"></span></span> have
+                         to say</h2>
+                     <div class="swiper position-relative"
+                         data-slider-options="{ &quot;autoHeight&quot;: true, &quot;loop&quot;: true, &quot;allowTouchMove&quot;: true, &quot;autoplay&quot;: { &quot;delay&quot;: 4000, &quot;disableOnInteraction&quot;: false }, &quot;navigation&quot;: { &quot;nextEl&quot;: &quot;.swiper-button-next&quot;, &quot;prevEl&quot;: &quot;.swiper-button-prev&quot; }, &quot;effect&quot;: &quot;fade&quot; }">
+                         <div class="swiper-wrapper mb-40px">
+
+                             <div class="swiper-slide review-style-08">
+                                 <p class="w-80 xl-w-90 lg-w-100">Love the theme, really neat and super easy to use. But
+                                     the customer support is what makes this an even greater theme! ThemeZaa solved all the
+                                     problems. I had with my custom settings.</p>
+                                 <div class="mt-20px">
+                                     <img class="rounded-circle w-95px h-95px me-15px" src="images/avtar-24.jpg" alt>
+                                     <div class="d-inline-block align-middle text-start">
+                                         <div class="text-dark-gray alt-font fs-20"><span class="fw-700">Alexander</span>
+                                             harvard</div>
+                                         <div class="review-star-icon fs-18">
+                                             <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
+                                                 class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
+                                                 class="fa fa-star-fill"></i>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+
+
+                             <div class="swiper-slide review-style-08">
+                                 <p class="w-80 xl-w-90 lg-w-100">I've purchased several themes over the years and this
+                                     theme, by far is absolutely amazing. It's built extremely well the back end was well
+                                     thought out the design options are endless.</p>
+                                 <div class="mt-20px">
+                                     <img class="rounded-circle w-95px h-95px me-15px" src="images/avtar-25.jpg" alt>
+                                     <div class="d-inline-block align-middle text-start">
+                                         <div class="text-dark-gray alt-font fs-20"><span class="fw-700">Shoko</span>
+                                             mugikura</div>
+                                         <div class="review-star-icon fs-18">
+                                             <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
+                                                 class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
+                                                 class="fa fa-star-fill"></i>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+
+
+                             <div class="swiper-slide review-style-08">
+                                 <p class="w-80 xl-w-90 lg-w-100">The theme is very valid and offers many possibilities of
+                                     customization to adapt it to your needs, but the thing I got the most out of it was the
+                                     relationship with the support team.</p>
+                                 <div class="mt-20px">
+                                     <img class="rounded-circle w-95px h-95px me-15px" src="images/avtar-26.jpg" alt>
+                                     <div class="d-inline-block align-middle text-start">
+                                         <div class="text-dark-gray alt-font fs-20"><span class="fw-700">Leonel</span>
+                                             mooney</div>
+                                         <div class="review-star-icon fs-18">
+                                             <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
+                                                 class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
+                                                 class="fa fa-star-fill"></i>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+
+                         </div>
+                         <div class="d-flex justify-content-center justify-content-lg-start">
+
+                             <div
+                                 class="slider-one-slide-prev-1 swiper-button-prev slider-navigation-style-04 border border-color-extra-medium-gray">
+                                 <i class="fa-solid fa-arrow-left icon-small text-dark-gray"></i></div>
+                             <div
+                                 class="slider-one-slide-next-1 swiper-button-next slider-navigation-style-04 border border-color-extra-medium-gray">
+                                 <i class="fa-solid fa-arrow-right icon-small text-dark-gray"></i></div>
+
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
 
 
      <section class="pt-0 pb-2 sm-pb-6 overflow-hidden mt-5" id="musteriSection">
