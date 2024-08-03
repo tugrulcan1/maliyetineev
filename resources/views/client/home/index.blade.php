d49853fe3f787beff4bed372de249f465710186a
Zehra Sena Akgül <zehrasenakgul@gmail.com>
2024-08-03 16:58:49 +0300

    "Updated client home index blade template: changed image source, rating, and review text; added loop to display comments; removed unnecessary sections and styles."

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -308,46 +308,44 @@
                  <div class="col-lg-6 text-end md-mb-50px"
                      data-anime="{ &quot;effect&quot;: &quot;slide&quot;, &quot;color&quot;: &quot;#262b35&quot;, &quot;direction&quot;:&quot;lr&quot;, &quot;easing&quot;: &quot;easeOutQuad&quot;, &quot;delay&quot;:50}">
                      <figure class="position-relative m-0">
-                         <img src="images/demo-real-estate-09.jpg" class="w-90 border-radius-6px" alt>
+                         <img src="ihttps://maliyetineevpanel.emlaksepette.com/uploads/28-1_Photo%20-%206.jpg" class="w-90 border-radius-6px" alt>
                          <figcaption
                              class="position-absolute bg-dark-gray border-radius-10px box-shadow-quadruple-large bottom-100px xs-bottom-minus-20px left-minus-30px md-left-0px w-230px xs-w-210px text-center last-paragraph-no-margin animation-float">
                              <div
                                  class="bg-white pt-35px pb-35px border-radius-8px mb-15px position-relative top-minus-1px">
-                                 <h1 class="fw-700 ls-minus-3px text-dark-gray mb-0 alt-font">4.8</h1>
+                                 <h1 class="fw-700 ls-minus-3px text-dark-gray mb-0 alt-font">4.9</h1>
                                  <div class="text-golden-yellow fs-18 ls-1px">
                                      <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
                                          class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
                                          class="fa fa-star-fill"></i>
                                  </div>
-                                 <span class="text-dark-gray d-block alt-font fw-600">2488 Reviews</span>
+                                 <span class="text-dark-gray d-block alt-font fw-600">{{ count($comments) }}
+                                     Değerlendirme</span>
                                  <div
                                      class="d-inline-block fs-12 text-uppercase bg-base-color ps-20px pe-20px lh-30 text-white border-radius-100px box-shadow-large">
-                                     Excellent score</div>
+                                     Maliyetine Ev</div>
                              </div>
-                             <img src="images/demo-real-estate-trustpilot.svg" class="h-30px mb-20px" alt />
                          </figcaption>
                      </figure>
                  </div>
                  <div class="col-lg-5 offset-lg-1 col-md-10 text-center text-lg-start"
                      data-anime="{ &quot;translateY&quot;: [0, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 1200, &quot;delay&quot;: 100, &quot;staggervalue&quot;: 150, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
-                     <span class="fs-20 d-inline-block mb-15px text-base-color">Clients feedback</span>
+                     <span class="fs-20 d-inline-block mb-15px text-base-color">Değerlendirmeler</span>
                      <h2 class="alt-font fw-500 text-dark-gray ls-minus-1px shadow-none" data-shadow-animation="true"
-                         data-animation-delay="700">Here is what our <span
-                             class="fw-700 text-highlight d-inline-block">client's<span
-                                 class="bg-base-color h-10px bottom-4px opacity-3 separator-animation"></span></span> have
-                         to say</h2>
+                         data-animation-delay="700">Bizim için<span
+                             class="fw-700 text-highlight d-inline-block"> müşterilerimizin <span
+                                 class="bg-base-color h-10px bottom-4px opacity-3 separator-animation"></span></span>neler söylüyor ?</h2>
                      <div class="swiper position-relative"
                          data-slider-options="{ &quot;autoHeight&quot;: true, &quot;loop&quot;: true, &quot;allowTouchMove&quot;: true, &quot;autoplay&quot;: { &quot;delay&quot;: 4000, &quot;disableOnInteraction&quot;: false }, &quot;navigation&quot;: { &quot;nextEl&quot;: &quot;.swiper-button-next&quot;, &quot;prevEl&quot;: &quot;.swiper-button-prev&quot; }, &quot;effect&quot;: &quot;fade&quot; }">
                          <div class="swiper-wrapper mb-40px">
+                            @foreach ($comments as $comment)
 
                              <div class="swiper-slide review-style-08">
-                                 <p class="w-80 xl-w-90 lg-w-100">Love the theme, really neat and super easy to use. But
-                                     the customer support is what makes this an even greater theme! ThemeZaa solved all the
-                                     problems. I had with my custom settings.</p>
+                                 <p class="w-80 xl-w-90 lg-w-100">{!! $comment->content !!}</p>
                                  <div class="mt-20px">
                                      <img class="rounded-circle w-95px h-95px me-15px" src="images/avtar-24.jpg" alt>
                                      <div class="d-inline-block align-middle text-start">
-                                         <div class="text-dark-gray alt-font fs-20"><span class="fw-700">Alexander</span>
+                                         <div class="text-dark-gray alt-font fs-20"><span class="fw-700">{{ $comment->full_name }}</span>
                                              harvard</div>
                                          <div class="review-star-icon fs-18">
                                              <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
@@ -357,54 +355,21 @@
                                      </div>
                                  </div>
                              </div>
+                             @endforeach
 
 
-                             <div class="swiper-slide review-style-08">
-                                 <p class="w-80 xl-w-90 lg-w-100">I've purchased several themes over the years and this
-                                     theme, by far is absolutely amazing. It's built extremely well the back end was well
-                                     thought out the design options are endless.</p>
-                                 <div class="mt-20px">
-                                     <img class="rounded-circle w-95px h-95px me-15px" src="images/avtar-25.jpg" alt>
-                                     <div class="d-inline-block align-middle text-start">
-                                         <div class="text-dark-gray alt-font fs-20"><span class="fw-700">Shoko</span>
-                                             mugikura</div>
-                                         <div class="review-star-icon fs-18">
-                                             <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
-                                                 class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
-                                                 class="fa fa-star-fill"></i>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>
-
-
-                             <div class="swiper-slide review-style-08">
-                                 <p class="w-80 xl-w-90 lg-w-100">The theme is very valid and offers many possibilities of
-                                     customization to adapt it to your needs, but the thing I got the most out of it was the
-                                     relationship with the support team.</p>
-                                 <div class="mt-20px">
-                                     <img class="rounded-circle w-95px h-95px me-15px" src="images/avtar-26.jpg" alt>
-                                     <div class="d-inline-block align-middle text-start">
-                                         <div class="text-dark-gray alt-font fs-20"><span class="fw-700">Leonel</span>
-                                             mooney</div>
-                                         <div class="review-star-icon fs-18">
-                                             <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
-                                                 class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
-                                                 class="fa fa-star-fill"></i>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>
 
                          </div>
                          <div class="d-flex justify-content-center justify-content-lg-start">
 
                              <div
                                  class="slider-one-slide-prev-1 swiper-button-prev slider-navigation-style-04 border border-color-extra-medium-gray">
-                                 <i class="fa-solid fa-arrow-left icon-small text-dark-gray"></i></div>
+                                 <i class="fa-solid fa-arrow-left icon-small text-dark-gray"></i>
+                             </div>
                              <div
                                  class="slider-one-slide-next-1 swiper-button-next slider-navigation-style-04 border border-color-extra-medium-gray">
-                                 <i class="fa-solid fa-arrow-right icon-small text-dark-gray"></i></div>
+                                 <i class="fa-solid fa-arrow-right icon-small text-dark-gray"></i>
+                             </div>
 
                          </div>
                      </div>
@@ -414,79 +379,6 @@
      </section>
 
 
-     <section class="pt-0 pb-2 sm-pb-6 overflow-hidden mt-5" id="musteriSection">
-         <div class="container" id="musteri">
-             <div class="row align-items-center justify-content-center">
-                 <div class="col-lg-12 appear anime-complete text-center" id="musteriYorum"
-                     data-anime="{ &quot;translateX&quot;: [50, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 1200, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 150, &quot;easing&quot;: &quot;easeOutQuad&quot; }"
-                     style="display:flex">
-                     <div class="review-star-icon fs-22 d-inline-block align-middle me-15px">
-                         <i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i><i
-                             class="fa fa-star-fill"></i><i class="fa fa-star-fill"></i>
-                     </div>
-                     <h6 class="text-dark-gra fw-600 mb-0 ls-minus-1px " id="musteriH6">Müşteri Yorumları</h6>
-                 </div>
-             </div>
-         </div>
-     </section>
-     <section class="m-0 p-0">
-         <div class="container p-0">
-
-
-             <div class="row m-0 align-items-center justify-content-center border border-color-extra-medium-gray  p-0  m-0 md-border-radius-6px ps-10px pe-10px box-shadow-quadruple-large"
-                 style="    padding: 0 !important;
-    margin: 0 !important;
-    border-radius: 10px;"
-                 data-anime="{ &quot;scale&quot;: [1.1, 1], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 800, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
-                 <div class="col-lg-10">
-                     <div class="swiper slider-one-slide testimonials-style-09"
-                         data-slider-options="{ &quot;slidesPerView&quot;: 1, &quot;loop&quot;: true, &quot;autoplay&quot;: { &quot;delay&quot;: 4000, &quot;disableOnInteraction&quot;: false }, &quot;navigation&quot;: { &quot;nextEl&quot;: &quot;.slider-one-slide-next-1&quot;, &quot;prevEl&quot;: &quot;.slider-one-slide-prev-1&quot; }, &quot;keyboard&quot;: { &quot;enabled&quot;: true, &quot;onlyInViewport&quot;: true }, &quot;effect&quot;: &quot;slide&quot; }">
-                         <div class="swiper-wrapper align-items-center">
-
-                             @foreach ($comments as $comment)
-                                 <div class="swiper-slide">
-                                     <div class="row align-items-center pt-25px pb-25px">
-                                         <div class="col-lg-8 d-lg-flex align-items-center text-center text-lg-start">
-
-                                             <span style=""
-                                                 class="d-block ps-40px md-ps-0 md-mx-auto position-relative">{!! $comment->content !!}</span>
-                                         </div>
-                                         <div class="col-lg-1 d-none d-lg-inline-block">
-                                             <div
-                                                 class="separator-line w-1px md-w-100 h-60px md-h-1px bg-extra-medium-gray mx-auto">
-                                             </div>
-                                         </div>
-                                         <div class="col-lg-3 text-center text-lg-start md-mt-15px">
-                                             <span
-                                                 class="fs-19 ls-minus-05px fw-600 text-dark-gray d-block lh-28">{{ $comment->full_name }}</span>
-                                             <div></div>
-                                         </div>
-                                     </div>
-                                 </div>
-                             @endforeach
-
-
-
-                         </div>
-                     </div>
-                 </div>
-                 <div class="col-lg-2 md-mb-25px">
-                     <div class="d-flex justify-content-center">
-
-                         <div
-                             class="slider-one-slide-prev-1 swiper-button-prev slider-navigation-style-04 bg-very-light-gray">
-                             <i class="fa-solid fa-arrow-left icon-small text-dark-gray"></i>
-                         </div>
-                         <div
-                             class="slider-one-slide-next-1 swiper-button-next slider-navigation-style-04 bg-very-light-gray">
-                             <i class="fa-solid fa-arrow-right icon-small text-dark-gray"></i>
-                         </div>
-
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </section>
 
      <section>
          <div class="container" style="overflow: hidden;">
